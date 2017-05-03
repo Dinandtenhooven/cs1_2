bcc526c1479306c68c84e1282d056f7302e68016
Dinandtenhooven <DRtenhooven@gmail.com>
2017-05-03 14:02:58 +0200

    Update AlexWalker.java
    fuck alex

--- AlexWalker.java
+++ AlexWalker.java
@@ -1,14 +1,27 @@
-/**
- * This class is useless
- * 
- * TODO: Try to not be such a massive prick
- */
-public class AlexWalker {
-	public AlexWalker(){
-		// take a dump
-	}
 
-	public void interact() {
-		//NEVER Call this function
+/**
+ * 	Alex Walker is a massive cunt!
+ **/
+
+public class AlexWalker {
+	// this class is useless
+	public AlexWalker {
+		Jokes.put(0, "Hee boys, grades are up.");
+		Jokes.put(1, "Ow man, i just finished the 10.000.000 words essay for tomorrow.");
 	}
+	
+	
+	public static final String name = "Fucking wanker!";
+	
+	private static HashMap<Integer, String> Jokes = new HashMap<Integer, String>();
+	
+	public void getJoke() {
+		int size = myHashSet.size();
+		int item = new Random().nextInt(size);
+		
+		System.out.println(Jokes.get(item));
+	}
+	
+	
+	
 }
